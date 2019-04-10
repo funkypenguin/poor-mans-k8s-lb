479af68503a1affe701274b6df062b2cb011bb20
David Young <davidy@funkypenguin.co.nz>
2019-04-10 15:06:20 +1200

    Add ability to run once

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ARG VCS_REF
 
 # Good docker practice, plus we get microbadger badges
 LABEL org.label-schema.build-date=$BUILD_DATE \
-      org.label-schema.vcs-url="https://github.com/poor-mans-k8s-lb/turtlecoind.git" \
+      org.label-schema.vcs-url="https://github.com/funkypenguin/poor-mans-k8s-lb.git" \
       org.label-schema.vcs-ref=$VCS_REF \
       org.label-schema.schema-version="2.2-r1"
 
--- update-lb.sh
+++ update-lb.sh
@@ -9,17 +9,30 @@ WEBHOOK_TOKEN=${WEBHOOK_TOKEN}
 FRONTEND_PORT=${FRONTEND_PORT}
 BACKEND_PORT=${BACKEND_PORT}
 NAME=${NAME}
+SEND_PROXY=${SEND_PROXY}
 
 # We allow specifying action as an argument, since we might want to run a "delete" action on shutdown
 ACTION=$1
 
-while true; do
-   # Call the hook
-   curl \
-	-H "Content-Type:application/json" \
-	-H "X-Funkypenguin-Token:$WEBHOOK_TOKEN" \
-	-X POST \
-	-d "{\"dst-ip\":\"$DST_IP\",\"frontend-port\":\"$FRONTEND_PORT\",\"backend-port\":\"$BACKEND_PORT\",\"action\":\"$ACTION\",\"name\":\"$NAME\"}" \
-	$WEBHOOK
-   sleep $REPEAT_INTERVAL
-done
+# Allow running the webhook only once, used as an init container
+if [ "$REPEAT_INTERVAL" == "once" ]
+then
+  # Call the hook
+  curl \
+ -H "Content-Type:application/json" \
+ -H "X-Funkypenguin-Token:$WEBHOOK_TOKEN" \
+ -X POST \
+ -d "{\"dst-ip\":\"$DST_IP\",\"frontend-port\":\"$FRONTEND_PORT\",\"backend-port\":\"$BACKEND_PORT\",\"action\":\"$ACTION\",\"name\":\"$NAME\",\"send_proxy\":\"$SEND_PROXY\"}" \
+ $WEBHOOK
+else
+  while true; do
+     # Call the hook
+     curl \
+  	-H "Content-Type:application/json" \
+  	-H "X-Funkypenguin-Token:$WEBHOOK_TOKEN" \
+  	-X POST \
+  	-d "{\"dst-ip\":\"$DST_IP\",\"frontend-port\":\"$FRONTEND_PORT\",\"backend-port\":\"$BACKEND_PORT\",\"action\":\"$ACTION\",\"name\":\"$NAME\",\"send_proxy\":\"$SEND_PROXY\"}" \
+  	$WEBHOOK
+     sleep $REPEAT_INTERVAL
+  done
+fi
